f37e9c9ec28bcb82b83400b3c63976792a312232
Marc <marc.gluyas@gmail.com>
2017-09-15 21:44:03 +1200

    Added some base game structures

---
+++ Assets/Scripts/Scripts/Entity.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Component for objects to move around within the world
+/// Provides a simpler movement model than Unity's physics engine.
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class Entity : MonoBehaviour
+{
+	public readonly UnityEvent OnDeath = new UnityEvent();
+	public readonly UnityEvent OnDamage = new UnityEvent();
+	public readonly UnityEvent OnFall = new UnityEvent();
+	
+	public float MovementSpeed;
+	public int MaxHp;
+
+	public Vector2 Velocity { get; private set; }
+	public int Hp { get; private set; }
+
+	private Vector2 _facing = Vector2.up;
+
+	private bool _dead = false;
+
+	private Rigidbody2D _rb;
+	
+	private void Start()
+	{
+		Hp = MaxHp;
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	public Vector3 Facing
+	{
+		get { return _facing; }
+		set
+		{
+			_facing = value.normalized;
+			transform.rotation = Quaternion.FromToRotation(Vector3.forward, _facing);
+		}
+	}
+
+	/// <summary>
+	/// Set the Character's horizontal movement
+	/// </summary>
+	/// <param name="movement">Vector to move in, as a coefficient of the Character's movement speed</param>
+	/// <param name="truncate">If true, then the new speed will not exceed MovemenSpeed</param>
+	public void SetMovement(Vector2 movement, bool truncate = true)
+	{
+		if (truncate && movement.magnitude > 1)
+		{
+			movement.Normalize();
+		}
+		movement *= MovementSpeed;
+		Velocity = movement;
+	}
+	
+	/// <summary>
+	/// Adjust the Character's movement. Effectively acceleration
+	/// </summary>
+	/// <param name="delta">Vector to adjust movment in, as a coefficient of the Character's movement speed</param>
+	/// <param name="truncate">If true, then the new speed will not exceed MovemenSpeed</param>
+	public void AddMovement(Vector2 delta, bool truncate = true)
+	{
+		SetMovement(Velocity + delta, truncate);	
+	}
+
+	public void Damage(int damage)
+	{
+		Hp -= damage;
+		OnDamage.Invoke();
+		if (Hp <= 0 && !_dead)
+		{
+			OnDeath.Invoke();
+			_dead = true;
+		}
+	}
+	
+	private void FixedUpdate()
+	{
+		_rb.MovePosition(_rb.position + Velocity * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Scripts/Player.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Entity))]
+public class Player : MonoBehaviour
+{
+	public static Player Instance { get; private set; }
+	
+	private Entity _ent;
+
+	public GameObject Bullet;
+
+	public AudioSource SoundRapid;
+	public AudioSource SoundBurst;
+	public AudioSource SoundDamage;
+
+	public float MuzzleLength;
+	public float AimDeadZone = 0.1f;
+	
+	public float FireRateMean;
+	public float FireBurstSize;
+	public AnimationCurve FireStrengthCurve = AnimationCurve.Linear(0, 0, 1, 1);
+	
+	public float SpreadBase;
+	public float SpreadIncreaseFactor;
+	public float JitterBase;
+	public float JitterIncreaseFactor;
+	
+	public int AmmoMax;
+	public float AmmoRegenMax;
+	public float AmmoRegenMin;
+	public float AmmoRegenMaxPoint;
+
+	public int Hp
+	{
+		get { return _ent.Hp; }
+	}
+	
+	public int Ammo
+	{
+		get { return Mathf.FloorToInt(AmmoRaw); }
+	}
+	
+	public float AmmoRaw { get; private set; }
+	
+	public float FireRate { get; private set; }
+
+	private float _shotCarryOver;
+	private bool _burstFired = false;
+
+	private void Start()
+	{
+		Instance = this;
+		_ent = GetComponent<Entity>();
+		_ent.OnDeath.AddListener(Kill);
+		_ent.OnDamage.AddListener(() => SoundDamage.Play());
+		AmmoRaw = AmmoMax;
+	}
+	
+	private void Update()
+	{		
+		// MOVEMENT
+		_ent.SetMovement(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+		
+//		// AMMO REGEN
+//		{
+//			var regenRate = Mathf.Clamp(AmmoRaw / AmmoMax / AmmoRegenMaxPoint, 0, 1);
+//			AmmoRaw += Mathf.Lerp(AmmoRegenMin, AmmoRegenMax, regenRate) * Time.deltaTime;
+//			if (AmmoRaw >= AmmoMax) AmmoRaw = AmmoMax;
+//		}
+//		
+//		// SHOOTING
+//		{
+//			var aim = GetAim();
+//			if (Math.Abs(aim.magnitude) > AimDeadZone)	// set aim direction
+//			{
+//				_char.Facing = new Vector3(aim.x, 0, aim.y).normalized;
+//			}
+//			
+//			if (Input.GetButton("FireBurst"))
+//			{
+//				if (!_burstFired)
+//				{
+//					_shotCarryOver = AmmoRaw * FireBurstSize; // fire everything
+//					SoundBurst.Play();
+//				}
+//				_burstFired = true;
+//			}
+//			else
+//			{
+//				_burstFired = false;
+//				_shotCarryOver += FireRateMean * Time.deltaTime *
+//				                  FireStrengthCurve.Evaluate(Input.GetAxis("FireStrength"));
+//				if (_shotCarryOver > AmmoRaw) _shotCarryOver = AmmoRaw;
+//			}
+//			
+//			var shots = Mathf.FloorToInt(_shotCarryOver);
+//			AmmoRaw -= shots;
+//			_shotCarryOver -= shots;	// carry unfired 'half-shots' over between frames
+//
+//			var spread = SpreadBase;
+//			var jitter = JitterBase;
+//			
+//			for (var i = 0; i < shots; i++)
+//			{
+//				var bullet = Instantiate(Bullet).GetComponent<Bullet>();
+//				var aimSpread = Quaternion.AngleAxis(Random.Range(-spread, spread), Vector3.up) * _char.Facing;
+//				var posJitter = transform.position + aimSpread * Random.Range(MuzzleLength, jitter);
+//				bullet.Init(posJitter, aimSpread, _char);
+//	
+//				spread += SpreadBase * SpreadIncreaseFactor;
+//				jitter += JitterBase * JitterIncreaseFactor;
+//				
+//				if (!_burstFired) SoundRapid.Play();
+//			}
+//		}
+	}
+
+//	private Vector2 GetAim()
+//	{
+//		if (Ui.UsingMouse)
+//		{
+//			var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+//			var playerPlane = new Plane(Vector3.up, transform.position);
+//			
+//			float distance;
+//			if (playerPlane.Raycast(mouseRay, out distance))
+//			{
+//				var aim = mouseRay.GetPoint(distance) - transform.position;
+//				return new Vector2(aim.x, aim.z);
+//			}
+//			else
+//			{
+//				return Vector2.zero;
+//			}
+//		}
+//		else
+//		{
+//			return new Vector2(Input.GetAxis("AimX"), Input.GetAxis("AimY"));
+//		}
+//	}
+//	
+	private void Kill()
+	{
+		_ent.SetMovement(Vector3.zero);		
+		this.enabled = false;
+		GetComponent<Collider>().enabled = false;
+	}
+	
+	private void OnDestroy()
+	{
+		Instance = null;
+	}
+}
