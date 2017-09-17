e03c693a0e3d596583c9073607f462a92a86483b
Jackson Preston <jrainbowpreston@gmail.com>
2017-09-17 14:20:38 +1200

    HUD

--- Assets/Scripts/Bomber.cs
+++ Assets/Scripts/Bomber.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Bomber : MonoBehaviour
 {
 	public GameObject Bomb;
+	public float bombDelay;
+	private bool bombAvailable = true;
 	
 	private void Update () {
-		if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0) && bombAvailable)
 		{
+			bombAvailable = false;
+			StartCoroutine ("Recharge");
 			var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 			var playerPlane = new Plane(new Vector3(0, 0, 1), Vector3.zero);
 			
@@ -20,5 +24,10 @@ public class Bomber : MonoBehaviour
 				bomb.transform.position = targetPos;
 			}
 		}
+	}
+
+	IEnumerator Recharge() {
+		yield return new WaitForSeconds (bombDelay);
+		bombAvailable = true;
 	}
 }
--- Assets/Scripts/DamageTrigger.cs
+++ Assets/Scripts/DamageTrigger.cs
@@ -7,12 +7,15 @@ public class DamageTrigger : MonoBehaviour
 {
 	public bool RemoveOnTrigger;
 	public int Damage;
+	public ParticleSystem Explosion;
 	
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		var entity = other.GetComponent<Entity>();
 		if (entity != null)
 		{
+			var dmgExplosion = Instantiate (Explosion);
+			dmgExplosion.transform.position = this.transform.position;
 			entity.Damage(Damage);
 			if (RemoveOnTrigger) Destroy(this.gameObject);
 		}
